bfd54eae1e86a611ab7db5796824b3a1b174b871
SUGIRA1 <67726231+SUGIRA1@users.noreply.github.com>
2021-12-19 21:27:15 +0200

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,13 +1,14 @@
-
+//This program will enable the user to input any positive number and it will generate the Factorial of the number.
 #include<stdio.h>  
 int main()    
 {    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
+ int i,num;    // Variables Declaration
+ int fact=1;   // We initiate the fact with one because the factorial can not be less than zero.
+ printf("Please Enter any number: ");    
+  scanf("%d",&num);    
+    for(i=1;i<=num;i++){    
       fact=fact*i;    
   }    
-  printf("Factorial of %d is: %d",number,fact);    
+  printf("The Factorial of %d is: %d",num,fact);    
 return 0;  
 }   
